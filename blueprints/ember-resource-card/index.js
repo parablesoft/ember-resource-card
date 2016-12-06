06f6fd52c67979d93fbbea5189806ae86b6069b9
Vic Amuso <vic@parablesoft.com>
2016-12-06 15:53:52 -0500

    afterInstall addition to default blueprint

---
+++ blueprints/ember-resource-card/index.js
@@ -0,0 +1,15 @@
+/*jshint node:true*/
+module.exports = {
+  description: ''
+
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  afterInstall: function(options) {
+    return this.addPackageToProject("ember-aupac-typeahead","2.1.1");
+  }
+};
